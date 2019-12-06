44af23aa622f66d661d6e076caf9450948564aa8
山本卓実 <yamamototakumi@koumotoukeinoMacBook-Pro.local>
2019-12-06 23:10:13 +0900

    session5完了

--- section5.js
+++ section5.js
@@ -21,3 +21,34 @@ let user2 = users.find(function(u) {
 })
 
 console.log(user2);
+
+function Car(model) {
+  this.model = model;
+}
+
+var cars = [
+  new Car('プリウス'),
+  new Car('ノート'),
+  new Car('アクア'),
+];
+
+let car = cars.find(function(car) {
+  return car.model === 'アクア';
+});
+
+console.log(car);
+
+let posts = [
+  { id: 1, title: '古い投稿'},
+  { id: 2, title: '新しい投稿'},
+];
+
+let comment = { postId: 2, content: 'イイネ！' }
+
+function postForComment(posts, comment) {
+  return posts.find(function(post) {
+    return post.id === comment.postId;
+  });
+};
+
+console.log(postForComment(posts, comment));
